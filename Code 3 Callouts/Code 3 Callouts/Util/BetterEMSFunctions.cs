339ce77d3f077adfb11cd99ccb647adaaacad745
RiverGrande <Redacted>
2018-12-13 17:15:46 -0500

    Add CustomBackup and BetterEMS integration
    This pull request introduces integration with CustomBackupLocations and BetterEMS for whenever backup is requested by Code 3 Callouts. The plugin will fall back to LSPDFR functions if one of these
    integrations is not running.

---
+++ Code 3 Callouts/Code 3 Callouts/Util/BetterEMSFunctions.cs
@@ -0,0 +1,21 @@
+﻿using Rage;
+using System;
+using BetterEMS;
+using System.Collections.Generic;
+
+class BetterEMSFunctions
+{
+
+    internal static void RequestBackup(Vector3 location, int count = 1)
+    {
+
+        if (count == 1)
+            BetterEMS.API.EMSFunctions.RespondToLocation(location);
+
+        else {
+            for (int loopCount = 0; loopCount < 1; loopCount++) {
+                BetterEMS.API.EMSFunctions.RespondToLocation(location);
+            }
+        }
+    }
+}
---
+++ Code 3 Callouts/Code 3 Callouts/Util/CustomBackupFunctions.cs
@@ -0,0 +1,23 @@
+﻿using Rage;
+using System;
+using CustomBackupLocations;
+using System.Collections.Generic;
+
+class CustomBackupFunctions
+{   
+
+    internal static void RequestBackup(Vector3 location, LSPD_First_Response.EBackupUnitType type, LSPD_First_Response.EBackupResponseType response, int count = 1)
+    {
+
+        BackupManager LocalManager = new BackupManager();
+
+        if (count == 1)
+            LocalManager.BackupRespond(location, type, response);
+
+        else {
+            for (int loopCount = 0; loopCount < 1; loopCount++) {
+                LocalManager.BackupRespond(location, type, response);
+            }
+        }
+    }
+}
