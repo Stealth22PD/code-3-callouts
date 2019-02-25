bdc0ad3c9723d6600b8d9a3336f9d4ef8a1d1fc3
RiverGrande <32559399+RiverGrande@users.noreply.github.com>
2019-02-25 11:52:03 -0500

    Updated RPH version requirement
    Updated to the oldest version supported by Stealth.Common.dll

--- Code 3 Callouts/Code 3 Callouts/Constants.cs
+++ Code 3 Callouts/Code 3 Callouts/Constants.cs
@@ -11,7 +11,7 @@ namespace Stealth.Plugins.Code3Callouts
         internal const int LCPDFRDownloadID = 8082;
 
         internal const string ReqCommonVersion = "2.0.6685.38422";
-        internal const string ReqRPHVersion = "0.52.1061.10387";
+        internal const string ReqRPHVersion = "0.62.1216.14731";
         internal const string ReqLSPDFRVersion = "0.4.39.22580";
         internal const string ReqRNUIVersion = "1.5.1.0";
     }
